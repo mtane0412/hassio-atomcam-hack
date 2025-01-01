372d13edd19889598c75644b3a6b07af36661330
Masayuki Tanenobu <mtane0412@gmail.com>
2025-01-01 22:05:22 +0900

    Update __init__.py

--- custom_components/atomcam_hack/__init__.py
+++ custom_components/atomcam_hack/__init__.py
@@ -103,7 +103,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         "ini": ini,
     }
 
-    hass.config_entries.async_setup_platforms(entry, _PLATFORMS)
+    await hass.config_entries.async_setup_platforms(entry, _PLATFORMS)
     return True
 
 
